a4832b377038fdc75a65e81ebe0ce3a8c621cca7
ElDaVz <162389925+ElDaVz@users.noreply.github.com>
2026-02-09 20:03:50 -0600

    Feature: new static method heapSort() and new tests for the static method heapSort()

--- src/app/Sorter.java
+++ src/app/Sorter.java
@@ -5,16 +5,46 @@ public class Sorter {
         //...
     }
 
-    public static int getMax(Integer[] array)  {
-        var max = array[0];
+    public static void heapify(int[] array, int length, int i) {
+        int leftChild = 2*i+1;
+        int rightChild = 2*i+2;
+        int largest = i;
 
-        for (int num : array) {
-            if (num > max) {
-                max = num;
-            }
+        // if the left child is larger than parent
+        if (leftChild < length && array[leftChild] > array[largest]) {
+            largest = leftChild;
         }
 
-        return max;
+        // if the right child is larger than parent
+        if (rightChild < length && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        // if a swap needs to occur
+        if (largest != i) {
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+            heapify(array, length, largest);
+        }
+    }
+
+    public static void heapSort(int[] array) {
+        if (array.length == 0) return;
+
+        // Building the heap
+        int length = array.length;
+        // we're going from the first non-leaf to the root
+        for (int i = length / 2 - 1; i >= 0; i--)
+            heapify(array, length, i);
+
+        for (int i = length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
     }
 
     public static void radixSort(Integer[] arr) {
