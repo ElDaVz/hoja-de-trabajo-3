7df57ffbe51fc0b1e340a4ea4dbcce4238c1814e
CrispoPlay <corellana.dieguez@gmail.com>
2026-02-09 22:49:30 -0600

    Feature: adding tests for Gnome, Quick and Merge Sort

---
+++ tests/QuickSortTests.java
@@ -0,0 +1,26 @@
+import app.RandomNumbersGenerator;
+import app.Sorter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QuickSortTests {
+
+    private Integer[] arr;
+
+    @BeforeEach
+    public void setUp() {
+        RandomNumbersGenerator generator = new RandomNumbersGenerator(70);
+        arr = generator.getArrayAsArray();
+    }
+
+    @Test
+    public void quickSort() {
+        Sorter.quickSort(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            assertTrue(arr[i] <= arr[i + 1], "Array should be sorted");
+        }
+    }
+}
