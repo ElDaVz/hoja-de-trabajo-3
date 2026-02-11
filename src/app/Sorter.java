d524bcdba30824d9a277659f381cee971328bfd7
ElDaVz <162389925+ElDaVz@users.noreply.github.com>
2026-02-10 18:35:28 -0600

    Fix: now static methods use generics

--- src/app/Sorter.java
+++ src/app/Sorter.java
@@ -1,11 +1,10 @@
 package app;
 
 public class Sorter {
-    private Sorter() {
-        //...
-    }
 
-    public static void gnomeSort(Integer[] arr) {
+    private Sorter() {}
+
+    public static <T extends Comparable<T>> void gnomeSort(T[] arr) {
         if (arr == null || arr.length == 0) return;
 
         int index = 0;
@@ -13,10 +12,10 @@ public class Sorter {
         while (index < arr.length) {
             if (index == 0) {
                 index++;
-            } else if (arr[index] >= arr[index - 1]) {
+            } else if (arr[index].compareTo(arr[index - 1]) >= 0) {
                 index++;
             } else {
-                int temp = arr[index];
+                T temp = arr[index];
                 arr[index] = arr[index - 1];
                 arr[index - 1] = temp;
                 index--;
@@ -24,12 +23,12 @@ public class Sorter {
         }
     }
 
-    public static void mergeSort(Integer[] arr) {
+    public static <T extends Comparable<T>> void mergeSort(T[] arr) {
         if (arr == null || arr.length <= 1) return;
         mergeSort(arr, 0, arr.length - 1);
     }
 
-    private static void mergeSort(Integer[] arr, int left, int right) {
+    private static <T extends Comparable<T>> void mergeSort(T[] arr, int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2;
 
@@ -40,40 +39,46 @@ public class Sorter {
         }
     }
 
-    private static void merge(Integer[] arr, int left, int mid, int right) {
+    private static <T extends Comparable<T>> void merge(T[] arr, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
 
-        Integer[] L = new Integer[n1];
-        Integer[] R = new Integer[n2];
+        Object[] L = new Object[n1];
+        Object[] R = new Object[n2];
 
         System.arraycopy(arr, left, L, 0, n1);
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
+        System.arraycopy(arr, mid + 1, R, 0, n2);
 
         int i = 0, j = 0, k = left;
 
         while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k++] = L[i++];
+            T leftVal = (T) L[i];
+            T rightVal = (T) R[j];
+
+            if (leftVal.compareTo(rightVal) <= 0) {
+                arr[k++] = leftVal;
+                i++;
             } else {
-                arr[k++] = R[j++];
+                arr[k++] = rightVal;
+                j++;
             }
         }
+
         while (i < n1) {
-            arr[k++] = L[i++];
+            arr[k++] = (T) L[i++];
         }
+
         while (j < n2) {
-            arr[k++] = R[j++];
+            arr[k++] = (T) R[j++];
         }
     }
 
-    public static void quickSort(Integer[] arr) {
+    public static <T extends Comparable<T>> void quickSort(T[] arr) {
         if (arr == null || arr.length <= 1) return;
         quickSort(arr, 0, arr.length - 1);
     }
 
-    private static void quickSort(Integer[] arr, int low, int high) {
+    private static <T extends Comparable<T>> void quickSort(T[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
 
@@ -82,26 +87,65 @@ public class Sorter {
         }
     }
 
-    private static int partition(Integer[] arr, int low, int high) {
-        int pivot = arr[high];
+    private static <T extends Comparable<T>> int partition(T[] arr, int low, int high) {
+        T pivot = arr[high];
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+            if (arr[j].compareTo(pivot) <= 0) {
                 i++;
-                int temp = arr[i];
+                T temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
 
-        int temp = arr[i + 1];
+        T temp = arr[i + 1];
         arr[i + 1] = arr[high];
         arr[high] = temp;
 
         return i + 1;
     }
 
+    public static <T extends Comparable<T>> void heapSort(T[] array) {
+        if (array == null || array.length == 0) return;
+
+        int length = array.length;
+
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            heapify(array, length, i);
+        }
+
+        for (int i = length - 1; i > 0; i--) {
+            T temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    public static <T extends Comparable<T>> void heapify(T[] array, int length, int i) {
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+        int largest = i;
+
+        if (leftChild < length && array[leftChild].compareTo(array[largest]) > 0) {
+            largest = leftChild;
+        }
+
+        if (rightChild < length && array[rightChild].compareTo(array[largest]) > 0) {
+            largest = rightChild;
+        }
+
+        if (largest != i) {
+            T temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+
+            heapify(array, length, largest);
+        }
+    }
 
     public static void radixSort(Integer[] arr) {
         if (arr == null || arr.length == 0) return;
@@ -110,7 +154,7 @@ public class Sorter {
         int[] temp = new int[n];
 
         for (int i = 0; i < n; i++) {
-            temp[i] = arr[i]; // unboxing seguro
+            temp[i] = arr[i];
         }
 
         int max = temp[0];
@@ -123,7 +167,7 @@ public class Sorter {
         }
 
         for (int i = 0; i < n; i++) {
-            arr[i] = temp[i]; // boxing de regreso
+            arr[i] = temp[i];
         }
     }
 
@@ -148,46 +192,4 @@ public class Sorter {
 
         System.arraycopy(output, 0, arr, 0, n);
     }
-
-        public static void heapify(Integer[] array, int length, int i) {
-        int leftChild = 2*i+1;
-        int rightChild = 2*i+2;
-        int largest = i;
-
-        if (leftChild < length && array[leftChild] > array[largest]) {
-            largest = leftChild;
-        }
-
-        if (rightChild < length && array[rightChild] > array[largest]) {
-            largest = rightChild;
-        }
-
-        if (largest != i) {
-            int temp = array[i];
-            array[i] = array[largest];
-            array[largest] = temp;
-            heapify(array, length, largest);
-        }
-    }
-
-    public static void heapSort(Integer[] array) {
-        if (array == null || array.length == 0) return;
-
-        int length = array.length;
-
-
-        for (int i = length / 2 - 1; i >= 0; i--)
-            heapify(array, length, i);
-
-        for (int i = length - 1; i >= 0; i--) {
-            int temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
-
-            heapify(array, i, 0);
-        }
-    }
-
-
-
 }
